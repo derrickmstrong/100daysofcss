20559733531f237efa66d7ddef3e70eb04ac8cf9
Derrick Strong <derrickmstrong@gmail.com>
2021-11-17 11:00:09 -0600

    Reformatted code

--- day2/app.js
+++ day2/app.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 
-container.addEventListener('click', function () {
+container.addEventListener('click', () => {
   container.classList.toggle('active');
   let noAnimation = document.querySelectorAll('.no-animation');
   noAnimation.forEach(item => {
